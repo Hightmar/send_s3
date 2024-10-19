36842d08085af87a44dda9190a8e852b676fdebe
Hightmar <antoine.lounis@proton.me>
2024-10-19 07:37:45 +0200

    Indent

--- send_s3.sh
+++ send_s3.sh
@@ -61,8 +61,8 @@ fi
 
 _EXE_NAME="aws --endpoint=${_S3_ENDPOINT}"
 
-for ((i=0; i < "${#fichiers[@]}"; i++)); do
-    x=$((i+1))
+for ((i = 0; i < "${#fichiers[@]}"; i++)); do
+    x=$((i + 1))
 
     _EXE_PARM="s3 cp ${fichiers[i]} s3://${_S3_BUCKET}/${_S3_FOLDER}/ --profile ${_S3_PROFILE} --no-verify-ssl --no-progress"
     ${_EXE_NAME} ${_EXE_PARM}
@@ -70,17 +70,17 @@ for ((i=0; i < "${#fichiers[@]}"; i++)); do
     _RESULT=$?
 
     if [ "${_RESULT}" -eq 0 ]; then
-	    mv "${fichiers[i]}" "${_FOLDER_DONE}"/
-      _FILENAME=$(basename "${fichiers[i]}")
-      echo "${x}/${#fichiers[@]}- Fichier ${_FILENAME} envoye sur le S3 et deplace dans /work/done";
+        mv "${fichiers[i]}" "${_FOLDER_DONE}"/
+        _FILENAME=$(basename "${fichiers[i]}")
+        echo "${x}/${#fichiers[@]}- Fichier ${_FILENAME} envoye sur le S3 et deplace dans /work/done"
     else
-	    _ERROR_COUNT=$((_ERROR_COUNT+1))
-      _FILENAME=$(basename "${fichiers[i]}")
-      echo "${x}/${#fichiers[@]}- ERREUR :  Fichier ${_FILENAME} non envoye et non deplace dans /work/done.";
+        _ERROR_COUNT=$((_ERROR_COUNT + 1))
+        _FILENAME=$(basename "${fichiers[i]}")
+        echo "${x}/${#fichiers[@]}- ERREUR :  Fichier ${_FILENAME} non envoye et non deplace dans /work/done."
     fi
 done
 
 if [ "${_ERROR_COUNT}" -gt 0 ]; then
-   echo "ERREUR detectee, voir outputs"
-   exit 3
+    echo "ERREUR detectee, voir outputs"
+    exit 3
 fi
